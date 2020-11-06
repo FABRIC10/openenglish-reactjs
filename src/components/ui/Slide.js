72ae51e57111ec0a2b35c2dfb63784a2fa5c33ed
Fabricio Guzzo <392155+FABRIC10@users.noreply.github.com>
2020-11-06 13:39:55 -0500

    ajuste slide

--- src/components/ui/Slide.js
+++ src/components/ui/Slide.js
@@ -1,43 +1,55 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export const Slide = () => {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+    
     return (
     <section className="container__slide">
 
         <div className="container">
+
             <div className="row">
                 <div className="col">
-                    <Carousel>
-
-                        <Carousel.Item>
-                            <img className="" src="assets/images/pics/pic01.png" alt="First slide" />
-                            <Carousel.Caption>
-                                <h3>Title 1</h3>
-                                </Carousel.Caption>
-                        </Carousel.Item>
-
-                        <Carousel.Item>
-                            <img className="" src="assets/images/pics/pic02.png" alt="First slide" />
-                            <Carousel.Caption>
-                                <h3>Title 2</h3>
-                                </Carousel.Caption>
-                        </Carousel.Item>
-                        
-                        <Carousel.Item>
-                            <img className="" src="assets/images/pics/pic03.png" alt="First slide" />
-                            <Carousel.Caption>
-                                <h3>Title 3</h3>
-                                </Carousel.Caption>
-                        </Carousel.Item>
-                        
-                        <Carousel.Item>
-                            <img className="" src="assets/images/pics/pic04.png" alt="First slide" />
-                            <Carousel.Caption>
-                                <h3>Title 4</h3>
-                                </Carousel.Caption>
-                        </Carousel.Item>
-                        
+                <Carousel
+                    swipeable={true}
+                    draggable={false}
+                    showDots={true}
+                    responsive={responsive}
+                    ssr={true} // means to render carousel on server-side.
+                    infinite={true}
+                    autoPlay={true}
+                    autoPlaySpeed={1000}
+                    keyBoardControl={true}
+                    customTransition="all .5"
+                    transitionDuration={2}
+                    containerClass="carousel-container"
+                    removeArrowOnDeviceType={["desktop", "tablet", "mobile"]}
+                    deviceType={responsive}
+                    dotListClass="custom-dot-list-style"
+                    itemClass="carousel-item-padding-40-px"
+                    >
+                    <div><img className="" src="assets/images/pics/pic01.png" alt="First slide" /></div>
+                    <div><img className="" src="assets/images/pics/pic02.png" alt="First slide" /></div>
+                    <div><img className="" src="assets/images/pics/pic03.png" alt="First slide" /></div>
+                    <div><img className="" src="assets/images/pics/pic04.png" alt="First slide" /></div>
                     </Carousel>
                 </div>
             </div>  
